228e3db67961f6ff7d04a9aa904765bb98fb2741
Vandriele Barbosa <43830415+Hellavn@users.noreply.github.com>
2022-11-02 00:05:32 -0300

    FrontEnd: adição de titulo e ico personalizado

--- GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
+++ GeekShopping/GeekShopping.Web/Utils/HttpClientExtensions.cs
@@ -12,5 +12,19 @@ namespace GeekShopping.Web.Utils
             var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         }
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = _contentType;
+            return httpClient.PostAsync(url, content);
+        }
+        public static Task<HttpResponseMessage> PutAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = _contentType;
+            return httpClient.PutAsync(url, content);
+        }
     }
 }

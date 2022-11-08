b35613a999085bc088d4f13982916c4db9c824bc
Vandriele Barbosa <43830415+Hellavn@users.noreply.github.com>
2022-11-08 20:43:22 -0300

    Implementando a classe IdentityConfiguration

---
+++ GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace GeekShopping.IdentityServer.Configuration
+{
+    public static class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Costumer = "Costumer";
+
+        // Criação da IdentityResource
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+    }
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope()
+        };
+}

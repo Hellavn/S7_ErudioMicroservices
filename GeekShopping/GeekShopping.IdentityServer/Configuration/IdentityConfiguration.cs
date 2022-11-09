5d3fb1d06d6a925e9e53598e49d747d926659568
Vandriele Barbosa <43830415+Hellavn@users.noreply.github.com>
2022-11-08 22:40:39 -0300

    Configurando Clients no IdentityServer

--- GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ GeekShopping/GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -1,13 +1,13 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using System.Collections.Generic;
 
 namespace GeekShopping.IdentityServer.Configuration
 {
     public static class IdentityConfiguration
     {
         public const string Admin = "Admin";
-        public const string Costumer = "Costumer";
-
-        // Criação da IdentityResource
+        public const string Client = "Client";
 
         public static IEnumerable<IdentityResource> IdentityResources =>
             new List<IdentityResource>
@@ -16,11 +16,34 @@ namespace GeekShopping.IdentityServer.Configuration
                 new IdentityResources.Email(),
                 new IdentityResources.Profile()
             };
-    }
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("geek_shopping", "GeekShopping Server"),
+                new ApiScope(name: "read", "Read data."),
+                new ApiScope(name: "write", "Write data."),
+                new ApiScope(name: "delete", "Delete data."),
+            };
+        public static IEnumerable<Client> Clientes =>
+            new List<Client>{
+                new Client
+                {
+                    ClientId = "Client",
+                    ClientSecrets = {new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"read", "write", "profile"}
 
-    public static IEnumerable<ApiScope> ApiScopes =>
-        new List<ApiScope>
-        {
-            new ApiScope()
-        };
+                },
+                new Client
+                {
+                    ClientId = "geek_shopping",
+                    ClientSecrets = {new Secret("my_super_secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = {"http://localhost:64382/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:64382/signout-callback-oids"},
+                    AllowedScopes = {"read", "write", "profile"}
+
+                }
+            };
+    }
 }
